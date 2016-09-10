399638064bd51c8d7435227a0d577558d1243dff
wangli <495661557@qq.com>
2016-09-10 12:26:57 +0800

    add a holle gui

--- hollegit.java
+++ hollegit.java
@@ -2,5 +2,6 @@ class  {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello World11111111111!");
+			System.out.println("Hello GUI");
 	}
 }
